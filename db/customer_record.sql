7dff85075e02ebfabd86ab9b8098d76c388f7ada
Mina Kim <70475010+eulneul@users.noreply.github.com>
2024-12-21 17:27:38 +0900

    Update customer_record.sql

--- db/customer_record.sql
+++ db/customer_record.sql
@@ -1,18 +1,10 @@
 CREATE TABLE customer_record
 (
+    customer_id varchar(50),
     nickname varchar(50),
-    content TEXT(2000),
+    content VARCHAR(2000),
     revisit_date date,
-    revisit INT,
     store_name varchar(50),
-
     store_type varchar(20),
-    google FLOAT,
-    baedal FLOAT,
-    mean_rate FLOAT,
-    review_count INT,
-
-    photo_count INT,
-    nickname_only varchar(20),
-    customer_id INT
+    revisit INT
 );
